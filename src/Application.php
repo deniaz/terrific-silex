6fea3f5f2c8c93d946a7a5d3d70cd00be49279a5
Robert Vogt <robert.vogt@namics.com>
2015-03-02 17:27:33 +0100

    Initial Commit

---
+++ src/Application.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace Deniaz\Splendid;
+
+use Deniaz\Splendid\Provider\RouteProvider;
+use Deniaz\Terrific\Twig\Extension\TerrificExtension;
+use Silex\Application as SilexApp;
+use Silex\Provider\TwigServiceProvider;
+
+/**
+ * Extends Silex Application with some must-have configurations.
+ *
+ * @package Deniaz\Splendid
+ * @author Robert Vogt <robert.vogt@namics.com>
+ */
+class Application extends SilexApp
+{
+    /**
+     * @var string Splendid Root Directory
+     */
+    private $rootDir;
+
+    /**
+     * Instantiate a new Application and configures Terrific-style routes and Twig.
+     *
+     * Objects and parameters can be passed as argument to the constructor.
+     *
+     * @param array $values The parameters or objects.
+     */
+    public function __construct(array $values = [])
+    {
+        parent::__construct($values);
+
+        $this['root_dir'] = realpath(__DIR__ . '/../') . '/';
+
+        $this['debug'] = true;
+        $this['tc.config'] = (is_readable($this->rootDir . 'config.json'))
+            ? json_decode(file_get_contents($this->rootDir . 'config.json'))
+            : null
+        ;
+
+        $this->register(
+            new TwigServiceProvider(),
+            [
+                'twig.path' => $this->rootDir . $this['tc.config']->micro->view_directory
+            ]
+        );
+
+        $app['twig'] = $this->share($this->extend('twig', function($twig, $app) {
+            $paths = [
+                $app['root_dir'] . $app['tc.config']->micro->view_partial_directory
+            ];
+            foreach ($app['tc.config']->micro->components as $component) {
+                $paths[] = $app['root_dir'] . $component->path;
+            }
+
+            $twig->addExtension(new TerrificExtension($paths, $app['tc.config']->micro->view_file_extension));
+            return $twig;
+        }));
+
+        $this->mount(
+            '/',
+            new RouteProvider(
+                json_decode(
+                    file_get_contents($this->rootDir . 'project/content.json'),
+                    true
+                )
+            )
+        );
+    }
+
+    /**
+     * @return string Splendid Root Directory
+     */
+    public function getRootDir()
+    {
+        return $this->rootDir;
+    }
+}
---
+++ src/Provider/RouteProvider.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Deniaz\Splendid\Provider;
+
+use Silex\Application;
+use Silex\ControllerCollection;
+use Silex\ControllerProviderInterface;
+use Symfony\Component\Config\Definition\Exception\Exception;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpKernel\HttpKernelInterface;
+use \Twig_Error_Loader;
+use \DomainException;
+
+/**
+ * Tries to render template from views directory.
+ *
+ * Class RouteProvider
+ * @package Deniaz\Splendid\Provider
+ */
+class RouteProvider implements ControllerProviderInterface
+{
+    /**
+     * @var array Mocked Content
+     */
+    private $content;
+
+    /**
+     * @param array $content Mocked Content
+     */
+    function __construct($content = [])
+    {
+        $this->content = $content;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function connect(Application $app)
+    {
+        /** @var ControllerCollection  $controllers */
+        $controllers = $app['controllers_factory'];
+
+        $controllers->get('/', function(Application $app) {
+            $subRequest = Request::create('/index', 'GET');
+            return $app->handle($subRequest, HttpKernelInterface::SUB_REQUEST);
+        });
+
+        $controllers->get('/{view}', function(Application $app, $view) {
+            $fileName = $view . $app['tc.config']->micro->view_file_extension;
+            try {
+                $data = (isset($this->content[$view])) ? $this->content[$view] : [];
+
+                return $app['twig']->render($fileName, $data);
+            } catch (Exception $e) {
+                throw new NotFoundHttpException($e->getMessage());
+            }
+        });
+
+        return $controllers;
+    }
+}
